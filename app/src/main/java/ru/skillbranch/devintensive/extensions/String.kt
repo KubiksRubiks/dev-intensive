8dbc3430bf8e9821241ed264f01d0c74be014620
Anna Zholud <ankazholud@yandex.ru>
2019-09-19 00:57:35 +0300

    *String.stripHtml
    Реализуй extension позволяющий очистить строку от html тегов и html escape последовательностей ("& < > ' ""), а так же удалить пустые символы (пробелы) между словами если их больше 1. Необходимо вернуть модифицированную строку
    Пример:
    "<p class="title">Образовательное IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch
    "<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,13 +1,20 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(value:Int = 16): String{
+fun String.truncate(value: Int = 16): String {
     val appendix = "..."
 
-    if(this.length>value){
-        var str = dropLast(this.length-value)
-        return (str+appendix)
+    if (this.length > value) {
+        var str = dropLast(this.length - value)
+        return (str + appendix)
     } else {
         return this
     }
+}
+
+fun String.stripHtml(): String {
+    return this
+        .replace(Regex("<[^>]*>"), "") //удаляет HTML-теги
+        .replace(Regex("\\s+"), " ") //удаляет пустые символы (пробелы) между словами если их больше 1
+        .replace(Regex("[&'\"]"), "") //удаляет html escape последовательности ("& < > ' "")
 
 }

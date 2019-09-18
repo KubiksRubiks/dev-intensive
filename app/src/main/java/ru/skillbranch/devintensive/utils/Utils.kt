997e96cd5f1035f2d4e06d98b828157378149f6e
Anna Zholud <ankazholud@yandex.ru>
2019-09-18 10:57:04 +0300

    *transliteration
    Реализуй метод Utils.transliteration(payload divider) принимающий в качестве аргумента строку (divider по умолчанию " ") и возвращающий преобразованную строку из латинских символов, словарь символов соответствия алфовитов доступен в ресурсах к заданию
    Пример:
    Utils.transliteration("Женя Стереотипов") //Zhenya Stereotipov
    Utils.transliteration("Amazing Петр","_") //Amazing_Petr

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -37,14 +37,51 @@ object Utils {
             } else{
                 if(it.isUpperCase()){
                     strBuilder.append(
-                        TransLiterators.replaceRusToEngLetter(it.toLowerCase().toString())
+                        replaceRusToEngLetter(it.toLowerCase().toString())
                         .toUpperCase()
                     )
                 } else {
-                    strBuilder.append(TransLiterators.replaceRusToEngLetter(it.toString()))
+                    strBuilder.append(replaceRusToEngLetter(it.toString()))
                 }
             }
         }
         return strBuilder.toString()
+    }
+
+    private fun replaceRusToEngLetter(letter: String): String = when (letter) {
+        "а" -> "a"
+        "б" -> "b"
+        "в" -> "v"
+        "г" -> "g"
+        "д" -> "d"
+        "е" -> "e"
+        "ё" -> "e"
+        "ж" -> "zh"
+        "з" -> "z"
+        "и" -> "i"
+        "й" -> "i"
+        "к" -> "k"
+        "л" -> "l"
+        "м" -> "m"
+        "н" -> "n"
+        "о" -> "o"
+        "п" -> "p"
+        "р" -> "r"
+        "с" -> "s"
+        "т" -> "t"
+        "у" -> "u"
+        "ф" -> "f"
+        "х" -> "h"
+        "ц" -> "c"
+        "ч" -> "ch"
+        "ш" -> "sh"
+        "щ" -> "sh'"
+        "ъ" -> ""
+        "ы" -> "i"
+        "ь" -> ""
+        "э" -> "e"
+        "ю" -> "yu"
+        "я" -> "ya"
+        else -> letter
     }
 }

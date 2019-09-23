1e47c37103bb47cc08031778c968d3e98bfaef94
Anna Zholud <ankazholud@yandex.ru>
2019-09-23 11:55:30 +0300

    **Activity.isKeyboardOpen Activity.isKeyboardClosed
    Реализуй extension для проверки, открыта или нет Software Keyboard с применением метода rootView.getWindowVisibleDisplayFrame(Rect())

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 
@@ -13,4 +15,21 @@ fun Activity.hideKeyboard() {
             it.hideSoftInputFromWindow(focus.windowToken, 0)
         }
     }
+}
+
+fun Activity.getRootView(): View {
+    return findViewById(android.R.id.content)
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val rootView: View = this.getRootView()
+    rootView.getWindowVisibleDisplayFrame(Rect())
+    val screenHeight = getRootView().height
+    val heightDiff = getRootView().height - (Rect().bottom - Rect().top)
+
+    return heightDiff > screenHeight / 3
+}
+
+fun Activity.isKeyboardClose():Boolean{
+    return !isKeyboardOpen()
 }

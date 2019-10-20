42bc4f09354d98a6a44f9f72093846043ee5d954
Anna Zholud <ankazholud@yandex.ru>
2019-10-20 20:18:41 +0300

    *Поиск по RecyclerView MainActivity
    Необходимо реализовать бизнес логику поиска чата по RecyclerView c применением SearchView
    
    Реализуй поиск по имени пользователей в списке, необходимо исспользовать menu_search с идентификатором элемента поиска R.id.action_search
    Ожидаемое повдение: при вводе в строку поиска символов на экран выводятся чаты чьи заголовки содержат введенный набор символов (с игнорированием регистра).При очистке поля ввода поискового запроса отображаются все чаты.

--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
@@ -1,8 +1,6 @@
 package ru.skillbranch.devintensive.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
@@ -12,14 +10,29 @@ class MainViewModel : ViewModel() {
     private val chatRepository = ChatRepository
 
     private val query = mutableLiveData("")
-    private val chats = Transformations.map(chatRepository.loadChats()) { chats ->
+    private val chats = chatRepository.loadChats()
+    private val chatItems = Transformations.map(chats) { chats ->
         chats.filter { !it.isArchived }
             .map { it.toChatItem() }
             .sortedBy { it.id.toInt() }
     }
 
     fun getChatData(): LiveData<List<ChatItem>> {
-        return chats
+        val result = MediatorLiveData<List<ChatItem>>()
+
+        val filter = {
+            val queryStr = query.value!!
+            val chats = chatItems.value!!
+
+            result.value =
+                if (!queryStr.isEmpty())
+                    chats.filter { it.title.contains(queryStr, true) }
+                else chats
+        }
+
+        result.addSource(chatItems) { filter.invoke() }
+        result.addSource(query) { filter.invoke() }
+        return result
     }
 
     fun addToArchive(chatId: String) {
